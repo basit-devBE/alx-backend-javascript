a5c43d56d354017d4e0e6deac58139d76af2eb87
basit <basitmohammed362@gmail.com>
2024-06-03 19:35:52 +0000

    4th commit

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,18 @@
 const updateStudentGradeByCity = (students, city, newGrades) => {
-    const defaultGrade = { grade: 'N/A' };
+  const defaultGrade = { grade: 'N/A' };
 
-    if (Array.isArray(students)) {
-        return students
-            .filter((student) => student.location === city)
-            .map((student) => ({
-                id: student.id,
-                firstName: student.firstName,
-                location: student.location,
-                grade: (newGrades.find((grade) => grade.studentId === student.id) || defaultGrade).grade
-            }));
-    }
+  if (Array.isArray(students)) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades.find((grade) => grade.studentId === student.id) || defaultGrade).grade,
+      }));
+  }
 
-    return [];
+  return [];
 };
 
 export default updateStudentGradeByCity;
