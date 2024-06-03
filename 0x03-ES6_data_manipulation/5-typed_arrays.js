c969e320462f689707a5dbcb2547f3c55f92351e
basit <basitmohammed362@gmail.com>
2024-06-03 19:47:32 +0000

    for typed_arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+const createIntTypedArray = (length, position, value) => {
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  const buffer = new DataView(new ArrayBuffer(length), 0, length);
+  buffer.setInt8(position, value);
+  return buffer;
+};
+
+export default createIntTypedArray;
