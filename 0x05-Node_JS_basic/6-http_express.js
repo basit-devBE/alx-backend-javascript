af6c003e2c3e933face5c7581ab03f6d0bcb1c4a
basit <basitmohammed362@gmail.com>
2024-07-22 19:09:18 +0000

    hello

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const app = express;
+
+const app = express();
 const PORT = 1245;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
