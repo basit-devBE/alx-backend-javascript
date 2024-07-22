76344eaa6c1fb13006a9627b1bf23eb8eca1bbe9
basit <basitmohammed362@gmail.com>
2024-07-22 19:41:57 +0000

    helo'

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,5 +1,5 @@
-import AppController  from "../controllers/AppController";
-import StudentsController from "../controllers/StudentsController";
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
 const mapRoutes = (app) => {
   app.get('/', AppController.getHomepage);
